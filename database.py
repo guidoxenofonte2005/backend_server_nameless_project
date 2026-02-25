9e3f016ec5901d85c45327641acc4458539a3ba0
Guido <guidoxenofonte@gmail.com>
2026-02-25 12:09:11 -0300

    fix engine

--- database.py
+++ database.py
@@ -8,7 +8,12 @@ load_dotenv()
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    connect_args={"sslmode": "require"},
+)
 LocalSession = sessionmaker(bind=engine)
 
 
